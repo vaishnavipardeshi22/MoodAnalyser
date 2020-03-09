b5baf6f78ebf76017faf446d26fbb1019809224c
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-09 15:06:57 +0530

    ADDED : Welcome message

---
+++ src/main/java/MoodAnalyser.java
@@ -0,0 +1,7 @@
+public class MoodAnalyser
+{
+    public static void main(String[] args)
+    {
+        System.out.println("******************** WELCOME TO MOOD ANALYSER ********************");
+    }
+}
